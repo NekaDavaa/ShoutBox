23c52a7a2993944b73a6aac96f8bebb68f9f1095
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-08-30 09:11:49 +0300

    Added session for error handler

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php 
-  include 'database.php';
+session_start();
+include 'database.php';
 ?>
+
 <?php
 $query = "select * from shouts order by id desc";
 $shouts = mysqli_query($con, $query);
 ?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -13,35 +16,39 @@ $shouts = mysqli_query($con, $query);
     <link href="css/style.css" rel="stylesheet" />
   </head>
   <body>
-   <div id="container">
-     <header>
+    <div id="container">
+      <header>
         <h1>Shout IT!</h1>
-    </header>
-   <div id="shouts">
-      <ul>
-        <?php while ($row = mysqli_fetch_assoc($shouts)) : ?>
-        <li class="shout"><span><span id="date-color"><?php echo $row['time']. ' -' ?> </span><span id="users-color"><?php echo $row['user']. ': ' ?></span><p><?php echo ucfirst($row['message']); ?></p>
-          <a href="process.php?action=delete&id=<?php echo $row['id']; ?>">Delete</a>
-        </li>
-      <?php endwhile ?>
+      </header>
+      <div id="shouts">
+        <ul>
+          <?php while ($row = mysqli_fetch_assoc($shouts)) : ?>
+          <li class="shout">
+            <span><span id="date-color"><?php echo $row['time']. ' -' ?> </span><span id="users-color"><?php echo $row['user']. ': ' ?></span><p><?php echo ucfirst($row['message']); ?></p>
+            <a href="process.php?action=delete&id=<?php echo $row['id']; ?>">Delete</a>
+          </li>
+          <?php endwhile ?>
         </ul>
-       </div>
-       <?php if (isset($_GET['error'])) : ?>
+      </div>
+      <?php if (isset($_SESSION['error'])) : ?>
         <div class="error-inputs">
-          <?php echo $_GET['error']; ?>
+          <?php 
+            echo $_SESSION['error']; 
+            unset($_SESSION['error']);  // Unset the session error after displaying it
+          ?>
         </div>
       <?php endif ?>
-  <div id="input">
-    <form method="post" action="process.php">
-     <input type="text" name="user" placeholder="Enter Your Name" />
-     <input type="text" name="message" placeholder="Enter a Message" />
-     <br />
-     <input class="shout-btn" type="submit" name="submit" value="Shout It Out" />
-     </form>
-  </div>
-    <div id="project-repo">
-    <a href="https://github.com/NekaDavaa/ShoutBox" target="_blank">Click on the link to overview the project code...</a>
+      <div id="input">
+        <form method="post" action="process.php">
+          <input type="text" name="user" placeholder="Enter Your Name" />
+          <input type="text" name="message" placeholder="Enter a Message" />
+          <br />
+          <input class="shout-btn" type="submit" name="submit" value="Shout It Out" />
+        </form>
+      </div>
+      <div id="project-repo">
+        <a href="https://github.com/NekaDavaa/ShoutBox" target="_blank">Click on the link to overview the project code...</a>
+      </div>
     </div>
-   </div>
   </body>
 </html>
--- process.php
+++ process.php
@@ -1,13 +1,12 @@
 <?php 
-
+session_start();
 include 'database.php';
-
 
 if(isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id'])) {
     $id = $_GET['id'];
     $query = "DELETE FROM shouts WHERE id = $id";
     
-// Verification
+    // Verification
     if (!mysqli_query($con, $query)) {
         die('Error: '. mysqli_error($con));
     } else {
@@ -16,37 +15,27 @@ if(isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id'])) 
     }
 }
 
-
-
 if (isset($_POST['submit'])) {
     $user = mysqli_real_escape_string($con, $_POST['user']);
     $message = mysqli_real_escape_string($con, $_POST['message']);
+    
     // Set Time zone
-    $time = date("d.m.y", time());       
+    $time = date("d.m.y", time());
+    
+    // Verification for filled inputs
+    if(empty($user) || empty($message)) {
+        $error = "Please fill inputs";
+        $_SESSION['error'] = $error;
+        header("Location: index.php");
+        exit();
+    } else {
+        $query = "INSERT INTO shouts (user, message, time) VALUES ('$user', '$message', '$time')";
+        
+        if(!mysqli_query($con, $query)) {
+            die('Error:' .mysqli_error($con));
+        } else {
+            header("Location: index.php");
+            exit();
+        }
+    }
 }
-
-// Verification for filled inputs
-if(!isset($user) || $user == '' || !isset($message) || $message == '') {
-	$error = "Please fill inputs";
-	header("Location: index.php?error=".urlencode($error));
-    exit();
-}
-
-else {
-
-	 // Insert query
-    $query = "INSERT INTO shouts (user, message, time) VALUES ('$user', '$message', '$time')";
-}
-
-
-// Verification to dont insert empty string
-if(!mysqli_query($con, $query)) {
-	die('Error:' .mysqli_error($con));
-}
-else {
-	header("Location: index.php");
-	exit();
-}
-
-
-?>
